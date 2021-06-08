d11f27575477597d29a08f3ab42232a1ed17e5f9
AlexRoca95 <Roca-VandeSype@hotmail.com>
2021-06-08 11:59:45 +0200

    Airplane texture loaded succesfully

---
+++ RESOURCES/Resources/Main.cpp
@@ -0,0 +1,37 @@
+#include "TextureHolder.h"
+
+
+
+int main()
+{	
+	TextureHolder textures;
+	textures.loadTexture(Textures::Airplane, "Media/Textures/Airplane.png"); // Load texture
+
+	sf::Sprite playerPlane;
+	playerPlane.setTexture(textures.get(Textures::Airplane));
+	playerPlane.setPosition(100.f, 100.f);
+
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My window with textures");
+
+	while(window.isOpen())
+	{
+		// Check if user wants to close window
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+
+		}
+
+
+		window.clear();
+		window.draw(playerPlane);
+		window.display();
+	}
+
+
+	return 0;
+}
+
---
+++ RESOURCES/Resources/TextureHolder.cpp
@@ -0,0 +1,46 @@
+#include "TextureHolder.h"
+
+using namespace sf;
+using namespace std;
+
+// Constructor
+TextureHolder::TextureHolder()
+{
+
+}
+
+// Method that loads a texture into our map.
+// It has two arguments: one with the ID of the texture and the second one with name of the file
+void TextureHolder::loadTexture(Textures::ID id, const std::string& pathFile)
+{
+	// Create a texture object and store it in a unique_ptr
+	unique_ptr<Texture> texture(new Texture());
+	texture->loadFromFile(pathFile);
+
+	// make_pair --> constructs a key-value pair for the map
+	mTextureMap.insert(make_pair(id, move(texture)));
+
+}
+
+// Method that returns a reference to a texture
+Texture& TextureHolder::get(Textures::ID id)
+{
+	// auto --> Keyword that automatically deduce the type of the initialization
+	//			(in this case type iterator)
+	// find --> Searchs in the map for an element with a key equivalent to id.
+	//			Returns an iterator if it is found or an interator to map::end
+	auto found = mTextureMap.find(id);
+
+	return *found->second;		// Returns the value of the pair key-value (2º one = value = texture)
+
+}
+
+// Method that returns a reference to a texture
+// Alternative get mehtod in case we only have a pointer o reference to a const texture
+const Texture& TextureHolder::get(Textures::ID id) const
+{
+	auto found = mTextureMap.find(id);
+
+	return *found->second;		
+
+}
