96d53f6a17e1f136324fe4646cd086241b54607d
AlexRoca95 <Roca-VandeSype@hotmail.com>
2021-06-22 11:33:30 +0200

    General class that can load resources to our game

--- RESOURCES/Resources/Main.cpp
+++ RESOURCES/Resources/Main.cpp
@@ -1,20 +1,23 @@
-#include "TextureHolder.h"
+#include "ResourceHolder.h"
 
 
 
 int main()
 {	
-	TextureHolder textures;
-	textures.loadTexture(Textures::Airplane, "Media/Textures/Airplane.png"); // Load Airplane texture
-	textures.loadTexture(Textures::Landscape, "Media/Textures/Desert.png"); // Load Dessert texture
-	textures.loadTexture(Textures::Missile, "Media/Textures/Missile.png");	// Load Missile texture
-
+	ResourceHolder<sf::Texture, Textures::ID> textures;
+	textures.load(Textures::Airplane, "Media/Textures/Airplane.png"); // Load Airplane texture
+	textures.load(Textures::Landscape, "Media/Textures/Desert.png"); // Load Dessert texture
+	textures.load(Textures::Missile, "Media/Textures/Missile.png");	// Load Missile texture
+	
 	sf::Sprite playerPlane;
+	sf::Sprite landScape;
 	playerPlane.setTexture(textures.get(Textures::Airplane));
+	landScape.setTexture(textures.get(Textures::Landscape));
 	playerPlane.setPosition(100.f, 100.f);
+	
+	sf::RenderWindow window(sf::VideoMode(640, 480), "My window with textures");
 
-	sf::RenderWindow window(sf::VideoMode(800, 600), "My window with textures");
-
+	
 	while(window.isOpen())
 	{
 		// Check if user wants to close window
@@ -29,10 +32,11 @@ int main()
 
 
 		window.clear();
+		window.draw(landScape);
 		window.draw(playerPlane);
 		window.display();
 	}
-
+	
 
 	return 0;
 }
